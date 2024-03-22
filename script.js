868df1255488177e89a126bc1fcc577c0c197cee
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-03-22 20:49:42 +0545

    Project is completed

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 "use strict";
 
-const randomNum = Math.floor(Math.random() * 20);
+let randomNum = Math.floor(Math.random() * 20);
 const button = document.querySelector(".check");
 const message = document.querySelector(".message");
 const userScore = document.querySelector(".score");
+const showResult = document.querySelector(".number");
+const body = document.querySelector("body");
+const reset = document.querySelector(".again");
+let highScore = Number(document.querySelector(".highscore").textContent);
+const highScoreWrap = document.querySelector(".highscore");
 
 let score = 20;
+
+const displayMessage = function(newMessage) {
+  message.textContent = newMessage;
+};
 
 button.addEventListener("click", () => {
   let guess = Number(document.querySelector(".guess").value);
   if (score > 0) {
     if (!guess) {
-      message.textContent = "Please enter number..";
-    } else if (guess > randomNum) {
-      message.textContent = `Your number ${guess} is too High`;
-    } else if (guess < randomNum) {
-      message.textContent = `Your number ${guess} is too Small`;
+      displayMessage("Please enter number..");
+    } else if (guess === randomNum) {
+      displayMessage(`Wow! Guess ${guess} is correct `);
+      showResult.textContent = `${guess}`;
+      body.style.backgroundColor = "#60b347";
+      showResult.style.width = "30rem";
+      if (score > highScore) {
+        highScore = score;
+        highScoreWrap.textContent = `${highScore}`;
+        console.log(highScore);
+      }
     } else {
-      message.textContent = `Wow! Your Guess ${guess} is correct `;
+      displayMessage(
+        randomNum < guess ? `${guess} Too high` : `${guess} Too low`
+      );
     }
     userScore.textContent = score;
   } else {
-    message.textContent = "You Lost The Game";
+    displayMessage("You Lost The Game");
   }
   score--;
+});
+
+reset.addEventListener("click", () => {
+  randomNum = Math.floor(Math.random() * 20);
+  displayMessage("Start guessing...");
+  body.style.backgroundColor = "#222";
+  showResult.style.width = "15rem";
+  showResult.textContent = "?";
+  document.querySelector(".guess").value = "";
+  score = 20;
+  userScore.textContent = score;
+  highScoreWrap.textContent = `${highScore}`;
 });
